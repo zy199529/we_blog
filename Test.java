de18f6d6dff93d521075deed90fc157fa29347b0
zy19950209 <823356427@qq.com>
2019-06-12 23:16:44 +0800

    java测试

---
+++ Test.java
@@ -0,0 +1,12 @@
+/* 
+* @Author: Lenovo
+* @Date:   2019-06-12 19:42:59
+* @Last Modified by:   Lenovo
+* @Last Modified time: 2019-06-12 21:45:37
+*/
+
+public class Test{
+    public static void main(String[] args) {
+    	System.out.println("hello java!");
+    }
+}
