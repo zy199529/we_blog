505fedc588e125ce3837ddf9200419437489bd45
zy19950209 <823356427@qq.com>
2019-07-18 23:28:53 +0800

    jsp文件添加

--- src/main/java/com/wmlbog/weblog/Hellocontroller.java
+++ src/main/java/com/wmlbog/weblog/Hellocontroller.java
@@ -1,10 +1,17 @@
 package com.wmlbog.weblog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.zip.DataFormatException;
+
+@Controller
+@RequestMapping(value = "/view")
 public class Hellocontroller{
 //    @Value("${name}")
 //    private String name;
@@ -14,12 +21,20 @@ public class Hellocontroller{
 //
 //    @Value("${content}")
 //    private String content;
-    @Autowired
-    private StudentProperties studentProperties;
+//    @Autowired
+//    private StudentProperties studentProperties;
     @RequestMapping("/hello")
+//    public String hello(Model m){
+//        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+//        return "hello";
+//    }
     public String hello(){
-        return studentProperties.getName()+studentProperties.getAge();
-    }
+//        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";
+}
+
+
+
 //    @RequestMapping("/hello")
 //    public String hello(){
 //        return content;
